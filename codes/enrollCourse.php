0aa3e54dff1c230113ee6b830f83627d6952921b
asmaalbarakati <albarakatiasma@hotmail.com>
2016-10-27 19:24:19 -0400

    add enroll course page

---
+++ codes/enrollCourse.php
@@ -0,0 +1,33 @@
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<?php
+
+include_once 'dbconnect.php';
+
+if(isset($_POST['enroll']))
+{
+
+    $tid = trim(mysql_real_escape_string($_POST['tid']));
+	$crChoice = trim(mysql_real_escape_string($_POST['course']));
+	$year = date("Y"); 
+	$card = trim(mysql_real_escape_string($_POST["card"]));
+	$add = trim(mysql_real_escape_string($_POST["address"]));
+	$ct = trim(mysql_real_escape_string($_POST["cardtype"]));
+	
+	//insert into the payemtn relation first..
+	$query1 = "INSERT INTO PAYMENT(cardnumber,type,address) values('$card','$ct','$add')";
+	$result = mysql_query($query1)
+		or die (mysql_error());
+		
+	//insert into the completion relation second..	
+	$query2 = "INSERT INTO COMPLETION(yearofcom,crid,tid,cardnumber) values('$year','$crChoice','$tid','$card')";
+	$result = mysql_query($query2)
+		or die (mysql_error());	
+		
+	require("header.html");	
+    echo ("<br><br><center><h2> You are registerd in the course.. Good Luck</h2></center>");	
+}	
+else
+	
+echo "Error";	
+		
+?>
