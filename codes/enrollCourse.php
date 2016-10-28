b13e54db19e5fa083d02e3bbc7316a4a3065abb1
asmaalbarakati <albarakatiasma@hotmail.com>
2016-10-27 20:23:35 -0400

    update enrollCourse php

--- codes/enrollCourse.php
+++ codes/enrollCourse.php
@@ -13,14 +13,16 @@ if(isset($_POST['enroll']))
 	$add = trim(mysql_real_escape_string($_POST["address"]));
 	$ct = trim(mysql_real_escape_string($_POST["cardtype"]));
 	
-	//insert into the payemtn relation first..
-	$query1 = "INSERT INTO PAYMENT(cardnumber,type,address) values('$card','$ct','$add')";
-	$result = mysql_query($query1)
-		or die (mysql_error());
+	//check if the card already exist, if not insert into the payemtn relation first..
+	$query1 = "SELECT * FROM PAYMENT WHERE cardnumber=$card";
+    if(@mysql_num_rows(mysql_query($query1)) != 1){	//not exist	insert it..
+	$query2 = "INSERT INTO PAYMENT(cardnumber,type,address) values('$card','$ct','$add')";
+	$result = mysql_query($query2)
+	or die (mysql_error());}
 		
 	//insert into the completion relation second..	
-	$query2 = "INSERT INTO COMPLETION(yearofcom,crid,tid,cardnumber) values('$year','$crChoice','$tid','$card')";
-	$result = mysql_query($query2)
+	$query3 = "INSERT INTO COMPLETION(yearofcom,crid,tid,cardnumber) values('$year','$crChoice','$tid','$card')";
+	$result = mysql_query($query3)
 		or die (mysql_error());	
 		
 	require("header.html");	
