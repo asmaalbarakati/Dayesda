7e6b38c6bc8b5a007dd04fa9974e9c3cc9de0cd7
asmaalbarakati <albarakatiasma@hotmail.com>
2016-10-28 12:58:42 -0400

    update SQL creation, insert queries of (organization, course, employee, service ) relations

---
+++ codes/srDetail.php
@@ -0,0 +1,17 @@
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<?php
+
+include_once 'dbconnect.php';
+			
+	require("header.html");
+	$query = "SELECT sname,sdescription FROM SERVICE";
+	$result = mysql_query($query)
+		or die (mysql_error());
+	    
+		echo("<br><br>");
+		while($record = mysql_fetch_array($result)){
+			echo ("<h4>$record[0]:</h4>");
+			echo ("<p>$record[1]</p> <br>");
+		}	
+		
+?>
