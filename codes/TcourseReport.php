199cb321a2784f759f986b106f90c63a81aaf7d6
asmaalbarakati <albarakatiasma@hotmail.com>
2016-11-25 13:23:24 -0500

    more modification
    add grade to trainee report

--- codes/TcourseReport.php
+++ codes/TcourseReport.php
@@ -14,7 +14,7 @@ if(isset($_POST['get']))
     $tid = trim(mysql_real_escape_string($_SESSION['user']));
 	
 	//Get all the courses of this specific Trainee
-	$query2 = "SELECT crname,yearofcom,fname,lname FROM Course c, COMPLETION cm, Employee e WHERE cm.tid='$tid' and cm.crid=c.crid and c.eid=e.eid order by yearofcom";
+	$query2 = "SELECT crname,yearofcom,grade,fname,lname FROM Course c, COMPLETION cm, Employee e WHERE cm.tid='$tid' and cm.crid=c.crid and c.eid=e.eid order by yearofcom";
 	$result = mysql_query($query2);
 	
 	if(@mysql_num_rows($result) != 0){
@@ -27,18 +27,21 @@ if(isset($_POST['get']))
         <tr>
         <th>Course Name</th>
         <th>Year</th>
+		<th>Grade</th>
 		<th>Instructor Name</th>
         </tr>
 		<?php
 			while($record = mysql_fetch_array($result)){
 			$name=$record[0];
 			$y=$record[1];
-			$fname=$record[2];
-			$lname=$record[3];
+			$g=$record[2];
+			$fname=$record[3];
+			$lname=$record[4];
 		?>	
 		<tr>
 					<td><?php echo $name; ?></td> 
 					<td><?php echo $y; ?></td> 
+					<td><?php echo $g; ?></td> 
 					<td><?php echo $fname." ".$lname; ?></td> 
 					
 		</tr>
