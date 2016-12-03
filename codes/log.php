cd97384e55e8d578e33a92994838a277198450c0
asmaalbarakati <albarakatiasma@hotmail.com>
2016-12-02 23:02:17 -0500

    organize code and last report

--- codes/log.php
+++ codes/log.php
@@ -25,7 +25,7 @@ if(isset($_POST['logIn']))
 		$_SESSION['user'] = $oid;
 		require("organization.html");	
 	}}
-	else if($val=="tra"){
+	elseif($val=="tra"){
 		
 		$tid = trim(mysql_real_escape_string($_POST['name']));
 		$pw = trim(mysql_real_escape_string($_POST['pw']));
@@ -43,7 +43,7 @@ if(isset($_POST['logIn']))
 		$_SESSION['user'] = $tid;
 		require("Trainee.html");	
 	}}
-	else if($val=="emp"){
+	else{
 		
 		$eid = trim(mysql_real_escape_string($_POST['name']));
 		$pw = trim(mysql_real_escape_string($_POST['pw']));
--- codes/register.php
+++ codes/register.php
@@ -8,17 +8,17 @@ if(isset($_POST["reg"]))
 	
 	if($val=="org"){
 		require("header.php");	
-		echo ("<br><br><center><h3>Welcome to Orgainzation Registeration</h3></center>");
+		echo ("<br><br><center><h3>Welcome to Orgainzation Registration</h3></center>");
 		echo ("<br><br><center><a id='butlink' href='OrganizationRegistration.php'>Register here</a></center>");
 	}
-	else if($val=="tra"){
+	elseif($val=="tra"){
 		require("header.php");
-		echo ("<br><br><center><h3>Welcome to Trainee Registeration</h3></center>");		
+		echo ("<br><br><center><h3>Welcome to Trainee Registration</h3></center>");		
 		echo ("<br><br><center><a id='butlink' href='TraineeReqistration.php'>Register here</a></center>");
 	}	
 	else{
 		require("header.php");	
-		echo ("<br><br><center><h3>Welcome to Employee Registeration</h3></center>");
+		echo ("<br><br><center><h3>Welcome to Employee Registration</h3></center>");
 		echo ("<br><br><center><a id='butlink' href='EmployeeRegistration.php'>Register here</a></center>");
 	}	
   }//end radiobutton options 	
